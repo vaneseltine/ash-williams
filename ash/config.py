f718d86c4b59f2774b0351a41dfe7c12716bf07b
Matt VanEseltine <vaneseltine@gmail.com>
2024-07-08 09:59:19 -0400

    Improve log_this

--- ash/config.py
+++ ash/config.py
@@ -1,6 +1,6 @@
 import logging
 from collections.abc import Callable
-from typing import Any
+from typing import Any, ParamSpec, TypeVar
 
 logging.basicConfig(
     level=logging.INFO,
@@ -10,13 +10,33 @@ logging.basicConfig(
     handlers=[logging.FileHandler("ash.log"), logging.StreamHandler()],
 )
 
+P = ParamSpec("P")
+R = TypeVar("R")
 
-def log_this(func: Callable[..., Any]) -> Callable[..., Any]:
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
+
+def log_this(func: Callable[P, R]) -> Callable[P, R]:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:  # pylint: disable=no-member
         logger = logging.getLogger(__name__)
-        logger.info(f"{func.__name__} | in  | {args}, {kwargs}")
+
+        incoming_to_log = log_inputs(args, kwargs)
+
+        logger.info(f"{func.__name__} | in  | {incoming_to_log}")
+
         result = func(*args, **kwargs)
-        logger.info(f"{func.__name__} | out | {result}")
+
+        logger.info(f"{func.__name__} | out | {trim(result)}")
+
         return result
 
     return wrapper
+
+
+def log_inputs(args: Any, kwargs: Any) -> str:
+    return ", ".join(trim(x) for x in [args, kwargs] if x)
+
+
+def trim(item: Any, maxlen: int = 160) -> str:
+    stringed = repr(item)
+    if not maxlen or len(stringed) <= maxlen:
+        return stringed
+    return stringed[: maxlen - 3] + "..."

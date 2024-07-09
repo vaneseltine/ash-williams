c917eb831b29e25e5b843e5fc656116a6c63bab0
Matt VanEseltine <matvan@umich.edu>
2024-07-09 15:22:20 -0400

    Consolidate tests

---
+++ test/test_ash.py
@@ -0,0 +1,197 @@
+from io import StringIO
+
+import pytest
+
+from ash.ash import DOI, BadDOIError, Paper, path_to_mime_type
+
+UNRETRACTED_TEXT = "A DOI here 10.21105/joss.03440 and that's all for now."
+UNRETRACTED_DOI = "10.21105/joss.03440"
+MOCKED_RETRACTION = "This is retracted 10.1234/retracted12349 in mock db."
+MOCKED_RETRACTION_DOI = "10.1234/retracted12349"
+
+
+class TestPaperDOIExtraction:
+
+    def test_create_via_string_with_mime(self):
+        paper = Paper(UNRETRACTED_TEXT, mime_type="text/plain")
+        assert set(paper.dois) == {UNRETRACTED_DOI}
+
+    def test_create_via_stringio_with_mime(self):
+        stringio = StringIO(UNRETRACTED_TEXT)
+        paper = Paper(stringio, mime_type="text/plain")
+        assert set(paper.dois) == {UNRETRACTED_DOI}
+
+    def test_create_via_pdf_without_suffix(self, vault):
+        paper = Paper.from_path(vault["pdf_without_suffix"])
+        assert set(paper.dois) == {"10.21105/joss.03440"}
+
+    @pytest.mark.parametrize(
+        "filename",
+        [
+            "basic_doi_colon.docx",
+            "basic_doi_colon.latex",
+            "basic_doi_colon.pdf",
+            "basic_doi_colon.rtf",
+            "basic_doi_colon.tex",
+            "basic_doi_colon.txt",
+            "basic_doi_url.docx",
+            "basic_doi_url.pdf",
+            "basic_doi_url.rtf",
+            "basic_doi_url.txt",
+        ],
+    )
+    def test_minimal_documents(self, vault, filename):
+        paper = Paper.from_path(vault[filename])
+        assert set(paper.dois) == {"10.21105/joss.03440"}
+
+    KNOWN = {
+        "Weeden_2023_Crisis.pdf": {
+            "10.1162/99608f92.151c41e3",
+            "10.1038/s41467-023-41111-1",
+            "10.1146/annurev-soc-060116-053450",
+            "10.1007/s12108-006-1006-8",
+            "10.1146/annurev-soc-090221-035954",
+            "10.1177/0038038519853146",
+            "10.1016/j.socscimed.2016.08.004",
+            "10.7298/PKWJ-GM89",
+        },
+        "42-1-orig_article_Cagney.pdf": {
+            "10.1016/S0140-6736(14)60921-1",
+            "10.1016/S0140-6736(14)62216-9",
+            "10.1016/S0140-6736(15)00342-6",
+            "10.1016/S0140-6736(16)00041-6",
+            "10.1016/S2213-2600(14)70125-0",
+            "10.1016/S2213-2600(14)70141-9.113",
+            "10.1016/S2213-2600(15)00005-3",
+            "10.1016/S2213-2600(15)00007-7",
+            "10.1038/478026a",
+            "10.1087/20110208",
+            "10.3109/08039488.2012.761401",
+        },
+        "pdf_without_suffix": {
+            "10.21105/joss.03440",
+        },
+    }
+
+    @pytest.mark.parametrize("filename, dois", KNOWN.items())
+    def test_pdf_vault_dois(self, vault, filename, dois):
+        paper = Paper.from_path(vault[filename])
+        assert set(paper.dois) == set(dois)
+
+    @pytest.mark.xfail(reason="Need to check for line breaks")
+    def test_line_break_shortens_doi_in_pdf(self, vault):
+        """
+        We get 10.3390/v130 instead
+        """
+        paper = Paper.from_path(vault["10.3389.fcvm.2021.745758.pdf"])
+        assert "10.3390/v13040700" in paper.dois
+
+    @pytest.mark.xfail(reason="Line breaks again")
+    def test_line_break_obscures_doi_in_pdf(self, vault):
+        """
+        This one isn't detected at all
+        """
+        paper = Paper.from_path(vault["42-1-orig_article_Cagney.pdf"])
+        assert "10.1016/S0140-6736(14)61033-3" in paper.dois
+
+
+class TestPaperReports:
+
+    def test_report_structure_unretracted(self, mock_db):
+        paper = Paper(UNRETRACTED_TEXT, mime_type="text/plain")
+        report = paper.report(mock_db)
+        assert report["dois"][UNRETRACTED_DOI] == {
+            "DOI is valid:": True,
+            "Retracted:": False,
+        }
+
+    def test_report_structure_retracted(self, mock_db):
+        paper = Paper(MOCKED_RETRACTION, mime_type="text/plain")
+        report = paper.report(mock_db)
+        assert report["dois"][MOCKED_RETRACTION_DOI] == {
+            "DOI is valid:": False,
+            "Retracted:": True,
+        }
+
+    def test_single_unretracted_doi_captured(self, mock_db):
+        paper = Paper(UNRETRACTED_TEXT, mime_type="text/plain")
+        report = paper.report(mock_db)
+        assert report["dois"][UNRETRACTED_DOI] == {
+            "DOI is valid:": True,
+            "Retracted:": False,
+        }
+
+
+class TestMIMEBehavior:
+    @pytest.mark.parametrize(
+        "filename, acceptable_mimes",
+        [
+            (
+                "basic_doi_colon.pdf",
+                ("application/pdf"),
+            ),
+            (
+                "pdf_without_suffix",
+                ("application/pdf"),
+            ),
+            (
+                "basic_doi_colon.docx",
+                "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            ),
+            (
+                "basic_doi_colon.rtf",
+                ("application/rtf", "application/msword"),
+            ),
+            (
+                "basic_doi_colon.txt",
+                ("text/plain"),
+            ),
+            (
+                "basic_doi_colon.latex",
+                ("application/x-latex"),
+            ),
+            (
+                "basic_doi_colon.tex",
+                ("text/x-tex", "application/x-tex"),
+            ),
+        ],
+    )
+    def test_mimes_detected_from_vault(self, vault, filename, acceptable_mimes):
+        path = vault[filename]
+        guessed_mime = path_to_mime_type(path)
+        assert guessed_mime in acceptable_mimes
+
+
+class TestDOI:
+
+    @pytest.mark.parametrize("raw", ["", None, "unavailable", "Unavailable", "1235.23"])
+    def test_dois_obviously_bad(self, raw):
+        with pytest.raises(BadDOIError):
+            _ = DOI(raw)
+
+    @pytest.mark.parametrize(
+        "raw", ["10.1234/retracted12345", "10.1126/science.aax5705"]
+    )
+    def test_dois_regex_acceptable(self, raw):
+        _ = DOI(raw)
+
+    def test_existence(self, mocker):
+        # Mock the 'request' method on the already instantiated 'http' instance in 'ash.ash'
+        mock_http_request = mocker.patch("ash.ash.http.request")
+
+        # Configure the mock to return a mock response object when called
+        mock_response = mocker.MagicMock()
+        mock_response.status = 200  # Presume a status code of 200 for the example
+        mock_http_request.return_value = mock_response
+
+        # Call the method that you expect to make the HTTP request
+        good_doi = "10.1126/science.aax5705"
+        doi = DOI(good_doi)
+        exists_result = doi.exists()
+
+        # Check if the endpoint meets your expectation
+        expected_url = "https://doi.org/api/handles/" + good_doi
+        mock_http_request.assert_called_once_with("HEAD", expected_url)
+
+        # Any other assertions regarding the return value of doi.exists() can follow...
+        assert exists_result  # Replace with the actual expected result

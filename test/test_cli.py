5c8f16c59ae0d9cba715f74ad2e290eac8239cd4
Matt VanEseltine <vaneseltine@gmail.com>
2025-08-31 21:25:31 -0400

    Fixed for Click test-breaking change
    Click changed exit code of implicit help to 2 rather than 0.

--- test/test_cli.py
+++ test/test_cli.py
@@ -6,10 +6,16 @@ from click.testing import CliRunner
 from ash import ash_cli
 
 
-@pytest.mark.parametrize("args", [[], ["--help"]])
-def test_help_works(args):
+@pytest.mark.parametrize(
+    "args, exit_code",
+    [
+        ([], 2),
+        (["--help"], 0),
+    ],
+)
+def test_help_works(args, exit_code):
     runner = CliRunner()
     result = runner.invoke(ash_cli, args)
-    assert result.exit_code == 0
+    assert result.exit_code == exit_code
     assert result.output.startswith("Usage")
     assert "help" in result.output
